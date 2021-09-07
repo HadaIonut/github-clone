d7187015165728d2d23cf50981452b12eab33167
Nora Leonte <nora.leonte@evozon.com>
2021-09-07 16:38:55 +0300

    filtering repos done

--- src/components/user/RepoPanel.vue
+++ src/components/user/RepoPanel.vue
@@ -1,16 +1,40 @@
-/* eslint-disable vue/valid-template-root */
 <template>
-  <div class="row pl-3">
-    <b-card v-for="(repo, index) in repos" :key="index" class="col-4 mr-2 mb-2">
-      <router-link :to="{ path: `/user/${username}/repo/${repo.name}` }">
-        <b-container class="py-3">
-          <h6 class="font-weight-bold text-left text-dark">
-            {{ repo.name }}
-          </h6>
-          <b-card-text class="text-left"> @{{ username }} </b-card-text>
-        </b-container>
-      </router-link>
-    </b-card>
+  <div>
+    <div>
+      <b-row>
+        <b-col cols="6" class="mb-3">
+          <b-input-group class="input">
+            <b-form-input
+              placeholder="Enter repo name"
+              v-on:input="handleFilter"
+            />
+            <b-input-group-append>
+              <b-input-group-text>
+                <b-icon icon="search" />
+              </b-input-group-text>
+            </b-input-group-append>
+          </b-input-group>
+        </b-col>
+      </b-row>
+    </div>
+    <div>
+      <b-row class="pl-3">
+        <b-card
+          v-for="(repo, index) in repos"
+          :key="index"
+          class="col-4 mr-2 mb-2"
+        >
+          <router-link :to="{ path: `/user/${username}/repo/${repo.name}` }">
+            <b-container class="py-3">
+              <h6 class="font-weight-bold text-left text-dark">
+                {{ repo.name }}
+              </h6>
+              <b-card-text class="text-left"> @{{ username }} </b-card-text>
+            </b-container>
+          </router-link>
+        </b-card>
+      </b-row>
+    </div>
   </div>
 </template>
 
@@ -22,16 +46,17 @@ export default {
   async created() {
     this.$store.dispatch('fetchRepos', this.username);
   },
-  methods: {},
+  methods: {
+    handleFilter(e) {
+      this.$store.dispatch('updateKeyword', e);
+      console.log(e);
+    },
+  },
   computed: {
     repos() {
-      return this.$store.getters.allRepos;
+      return this.$store.getters.filteredRepos;
     },
   },
 };
 </script>
-<style scoped>
-router-link {
-  color: white;
-}
-</style>
+<style scoped></style>

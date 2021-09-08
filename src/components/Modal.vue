69c44fcafd4d839aff1344e97c308de575456d97
Hada Ionut <hadaionut@gmail.com>
2021-09-08 17:10:13 +0300

    final touch-ups

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,28 +1,21 @@
 <template>
-  <b-modal size="lg" id="bv-modal-example" hide-footer class="vw-75">
-    <template #modal-title> Using <code>$bvModal</code> Methods </template>
+  <b-modal size="xl" id="bv-modal-example" hide-footer class="vw-75">
+    <template #modal-title> Using <code>$bvModal</code> Methods</template>
     <div class="d-block" v-highlight style="width:100%">
-      <!-- <vue-code-highlight language="javascript">
-        <pre>
-          {{ content }}
-        </pre>
-      </vue-code-highlight> -->
       <pre class="language-javascript">
-  <code>
-      {{ content }}
-  </code>
-</pre>
+        <code>
+          {{ content }}
+        </code>
+      </pre>
     </div>
   </b-modal>
 </template>
 
 <script>
-// import { component as VueCodeHighlight } from 'vue-code-highlight';
+import 'vue-code-highlight/themes/prism.css';
+
 export default {
   name: 'Modal',
-  components: {
-    // VueCodeHighlight,
-  },
   computed: {
     content() {
       return this.$store.getters.getFileContent;

57e94e6c0eb2d413aae1c4b08009964ab3cbf339
Krisztian Pinkovai <krisztian.pinkovai@evozon.com>
2021-09-09 11:57:19 +0300

    added skeleton loading for search results

---
+++ src/components/search/SearchResultsSkeletonCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="
+      d-flex
+      flex-row
+      justify-content-start
+      align-items-center
+      m-2
+      p-2
+      rounded
+      shadow
+      sizingVars
+      spacingVars
+    "
+  >
+    <b-skeleton type="avatar" class="imageSkeleton"></b-skeleton>
+    <b-skeleton class="ml-2 textSkeleton"></b-skeleton>
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchResultsSkeletonCard",
+};
+</script>
+<style scoped>
+.sizingVars {
+  --sizing-image: 64px
+}
+
+.spacingVars {
+  --margin-2: 0.5rem;
+}
+
+.textSkeleton {
+  width: calc(100% - var(--sizing-image) - var(--margin-2));
+}
+
+.imageSkeleton {
+  width: var(--sizing-image);
+  height: var(--sizing-image);
+}
+</style>

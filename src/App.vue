d704094a804ae700d0a69a281b8fcf57174e51e8
Nora Leonte <nora.leonte@evozon.com>
2021-09-09 16:18:05 +0300

    Created a navbar for the app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
+    <GlobalNavbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import GlobalNavbar from './components/GlobalNavbar';
+export default {
+  components: {
+    GlobalNavbar,
+  },
+};
+</script>
 
 <style src="./styles/global.css"></style>

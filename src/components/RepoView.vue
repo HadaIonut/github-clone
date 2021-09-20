3a13298cbfbdeb5a66dc0e79697c2e876585abb4
chira-cristian <chira.cristian621@gmail.com>
2021-09-20 14:12:10 +0300

    temp fix @click custom components

--- src/components/RepoView.vue
+++ src/components/RepoView.vue
@@ -1,29 +1,31 @@
 <template>
-
   <os-container class="pt-5 container">
-
     <div class="list-container">
       <h4 class="repo-title">{{ reponame }}</h4>
       <OsBreadcrumb>
-        <OsBreadcrumbItem
+        <div class="breadcrumb-item active" @click="goToLocation('')">
+          <!-- TODO: move to custom event -->
+          <div><i class="bi bi-house-fill"></i> Home</div>
+        </div>
+        <div
           v-for="(path, index) in paths"
           :key="index"
           @click="goToLocation(paths[index + 1])"
+          class="breadcrumb-item active"
         >
           {{ path }}
-        </OsBreadcrumbItem>
+        </div>
       </OsBreadcrumb>
       <LanguagesBar v-bind:username="username" v-bind:reponame="reponame" />
 
       <os-container-fluid class="p-0">
-
         <OsListGroupItem
-
           class="font-weight-bold bg-secondary text-white d-flex rounded-top"
         >
-          {{ commitsInfo }} commits have been made in this repository. {{nrOfBranches }} active
-          <span v-if="nrOfBranches == 1 ">&nbsp;branch </span>
-          <span v-if="nrOfBranches >1">&nbsp;branches</span>.
+          {{ commitsInfo }} commits have been made in this repository.
+          {{ nrOfBranches }} active
+          <span v-if="nrOfBranches == 1">&nbsp;branch </span>
+          <span v-if="nrOfBranches > 1">&nbsp;branches</span>.
         </OsListGroupItem>
         <OsListGroupItem
           class="clickable d-flex font-weight-bold"
@@ -33,24 +35,20 @@
           ..
         </OsListGroupItem>
         <OsListGroup class="list">
-          <OsListGroupItem
-            v-for="doc in docs"
-            :key="doc.sha"
-            class="list-item-container clickable"
-            @click="handleClick(doc)"
-          >
-            <div v-if="doc.type === 'dir'" class="list-item">
-              <i class="bi bi-folder-fill list-icon"> </i>
-              <div class="list-item-doc-title">{{ doc.name }}</div>
-            </div>
-            <div v-if="doc.type === 'file'" class="list-item">
-              <i class="bi bi-file-code list-icon"> </i>
-              <div class="list-item-doc-title">{{ doc.name }}</div>
-            </div>
-
-          </OsListGroupItem>
+          <div v-for="doc in docs" :key="doc.sha" v-on:click="handleClick(doc)">
+            <OsListGroupItem class="list-item-container clickable">
+              <div v-if="doc.type === 'dir'" class="list-item">
+                <i class="bi bi-folder-fill list-icon"> </i>
+                <div class="list-item-doc-title">{{ doc.name }}</div>
+              </div>
+              <div v-if="doc.type === 'file'" class="list-item">
+                <i class="bi bi-file-code list-icon"> </i>
+                <div class="list-item-doc-title">{{ doc.name }}</div>
+              </div>
+            </OsListGroupItem>
+          </div>
         </OsListGroup>
-       </os-container-fluid>
+      </os-container-fluid>
     </div>
     <!-- <Modal /> -->
   </os-container>
@@ -58,16 +56,24 @@
 
 <script>
 import LanguagesBar from './repos/LanguagesBar.vue';
-import OsBreadcrumbItem from './generics/OsBreadcrumbItem.vue';
+//import OsBreadcrumbItem from './generics/OsBreadcrumbItem.vue';
 import OsBreadcrumb from '../components/generics/OsBreadcrumb.vue';
-import OsContainer from './generics/Layout/OsContainer.vue'
-import OsContainerFluid from './generics/Layout/OsContainerFluid.vue'
+import OsContainer from './generics/Layout/OsContainer.vue';
+import OsContainerFluid from './generics/Layout/OsContainerFluid.vue';
 
 import OsListGroup from '../components/generics/OsListGroup.vue';
 import OsListGroupItem from '../components/generics/OsListGroupItem.vue';
 import { mapActions, mapGetters } from 'vuex';
 export default {
-  components: { LanguagesBar, OsBreadcrumb, OsBreadcrumbItem, OsContainer, OsContainerFluid,  OsListGroup, OsListGroupItem},
+  components: {
+    LanguagesBar,
+    OsBreadcrumb,
+    //OsBreadcrumbItem,
+    OsContainer,
+    OsContainerFluid,
+    OsListGroup,
+    OsListGroupItem,
+  },
   name: 'RepoView',
   props: {
     reponame: String,
@@ -166,9 +172,10 @@ export default {
       //this.$refs.myModal.show();*/
     },
     handleClick(doc) {
+      console.log('here');
       setTimeout(() => {
         if (doc.type === 'dir') this.updateRepoContents(doc.path);
-        //if (doc.type === 'file') this.openModal(doc.download_url, doc.name);
+        if (doc.type === 'file') this.openModal(doc.download_url, doc.name);
       }, 300);
     },
   },

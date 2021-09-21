7b9156fded23386d352d2f1c55c4c3c9946d3c5f
Hada Ionut <hadaionut@gmail.com>
2021-09-21 14:43:13 +0300

    Added osModal file

---
+++ src/components/generics/OsModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" >
+    <div :class="`modal-dialog ${size ? 'modal-' + size : ''} ${scrollable ? 'modal-dialog-scrollable' : ''}`">
+      <div class="modal-content">
+        <div class="modal-header" v-if="$slots.header">
+          <slot name="header"/>
+        </div>
+        <div class="modal-body">
+          <slot/>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer" v-if="$slots.footer"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OsModal',
+  props: {
+    size: String,
+    scrollable: Boolean
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
